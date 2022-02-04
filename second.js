9cf52b27a1d00ac013780d7fd84cc37192faf479
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-04 17:26:09 +0600

    second file added

---
+++ second.js
@@ -0,0 +1,14 @@
+// variable using let and const
+
+let num = 23;
+console.log(num);
+num = 25;
+console.log(num);
+// here we can change or update the value cause it let
+
+const num2 = 44;
+console.log(num2);
+
+// here we can't change the value of const cause it can't support to change
+//num2 = 22;
+//console.log(num2);
---
+++ third.js
@@ -0,0 +1,16 @@
+
+// simple math operation
+
+var Number1 = 12;
+var Number2 = 4;
+
+var total = Number1 + Number2;
+var sub = Number1 - Number2;
+var mul = Number1 * Number2;
+var div = Number1 / Number2;
+console.log('The sum is ',total);
+console.log('The subtraction is',sub);
+console.log('The multiplication is',mul);
+console.log('The division is ',div);
+
+

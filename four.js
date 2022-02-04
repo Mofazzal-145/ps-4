72de6dca7636d51b4d95846fc990606a903e4131
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-04 18:11:15 +0600

    conditionals

---
+++ four.js
@@ -0,0 +1,77 @@
+// comparison
+/* 
+const num1 = 23;
+const num2 = 121; */
+
+//-------codition 1
+
+/* if(num1 < num2) {
+    console.log('num1 is smaller');
+}
+else{
+ console.log('num2 is smaller ');
+} */
+// condition 2--- >
+
+/* const num1 = 230;
+const num2 = 121;
+
+if(num1 > num2) {
+    console.log('num1 is bigger');
+}
+else{
+ console.log('num2 is smaller ');
+} */
+
+// condition 3 <=
+
+const num1 = 250;
+const num2 = 223;
+
+if(num1 <= num2) { // || num1 == num2
+    console.log('both are same');
+}
+else{
+ console.log('Is not same ');
+}  
+
+// conditon 4 >=
+
+/* const num1 = 250;
+const num2 = 2230;
+
+if(num1 >= num2 ) { //|| num1 == num2
+    console.log('both are same');
+}
+else{
+ console.log('Is not same ');
+}
+  */
+
+// condition 5 
+
+/* const num1 = 250;
+const num2 = 25;
+
+if( num1 == num2) { 
+    console.log('both are same');
+}
+else{
+ console.log('Is not same ');
+}
+ */
+//c condition 6
+/* 
+ const num1 = 250;
+const num2 = 250;
+
+if( num1 != num2) { 
+    console.log('both are not same');
+}
+else{
+ console.log(' same ');
+} */
+ 
+
+
+
--- third.js
+++ third.js
@@ -8,10 +8,11 @@ var total = Number1 + Number2;
 var sub = Number1 - Number2;
 var mul = Number1 * Number2;
 var div = Number1 / Number2;
+var mod = Number1 % Number2;
 console.log('The sum is ',total);
 console.log('The subtraction is',sub);
 console.log('The multiplication is',mul);
 console.log('The division is ',div);
-
+console.log('The modulodivison is', mod);
 // addtion, subtraction, multiplication, and division
 

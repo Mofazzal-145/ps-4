b4758711e66efb93f9cb82faf1e7e1a96d056d90
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-04 17:13:17 +0600

    variable type

---
+++ first.js
@@ -0,0 +1,14 @@
+// write 3 variable type
+
+// Number, string boolean
+
+
+var Number = 23;
+console.log(typeof (Number));
+
+
+var string = 'mofazzal';
+console.log(typeof  (string));
+
+var isRaining  = true;
+console.log(typeof (isRaining));
